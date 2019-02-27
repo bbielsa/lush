2f51cb5a3b445900049597f1f591c977dc9c3310
bbielsa <bgb7@njit.edu>
2019-02-27 17:39:29 -0500

    Changed test.lua to test out enumerators

--- lib/test.lua
+++ lib/test.lua
@@ -1,11 +1,54 @@
 local iter = require('iter').iter
+local enumerator = require('iter').enumerator
 local fs = require('luvco.fs')
 
 
-local readdir_iter = fs.readdir('/')
+local numbers = coroutine.create(function()
+    for i = 1, 10 do
+        print("yielding", i)
+        coroutine.yield(i)
+    end
+end)
 
-for f in iter(readdir_iter) do
-    print('got here')
-    print(f)
+local double = n:map(function(v) return v * 2 end)
+
+for n in iter(double) do
+    print("got", n)
 end
+
+
+
+
+-- local numbers = {}
+
+-- for i = 1, 10 do 
+--     print("yielding", i)
+--     numbers[i] = i
+-- end
+
+
+-- for k, n in ipairs(numbers) do
+--     print("got", n)
+-- end
+
+-- local map = function(e, f)
+--     return enumerator.create(function(e, f)
+--         for v in iter(e) do
+--             enumerator.yield(f(v))
+--         end
+--     end)
+-- end
+
+-- local for_each = function(e, f)
+--     return enumerator.create(function(e, f)
+--         for v in iter(e) do
+--             f(e)
+--         end
+--     end)
+-- end
+
+-- local mapped = map(e, function(v) return v + 1 end)
+
+-- print(mapped.next())
+--for_each(mapped, function(v) print("v", v) end)
 
