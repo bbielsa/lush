99bc0f7f439c3323f94891cc24a2cd325348aa1f
bbielsa <bgb7@njit.edu>
2019-02-27 09:09:38 -0500

    Added ls builtin cmd

---
+++ src/cmd/ls/init.lua
@@ -0,0 +1,19 @@
+lcal uv = require('luv')
+
+function spy(...)
+    print(...)
+
+    return ...
+end
+
+uv.fs_scandir('/', function(err, req) 
+    local next = uv.fs_scandir_next(req)
+
+    while next do
+        print(next)
+
+        next = spy(uv.fs_scandir_next(req))
+    end
+end)
+
+uv.run('default')
