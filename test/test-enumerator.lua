ec36066f6026478fb5cb26b238d725d5580ab0ac
bbielsa <bgb7@njit.edu>
2019-03-02 11:40:12 -0500

    Added more unit tests for enumerators

--- test/test-enumerator.lua
+++ test/test-enumerator.lua
@@ -6,7 +6,7 @@ local iter = require('enumerator').iter
 
 
 describe('Enumerators', function()
-    it('should iterate over tables', function()
+    it('should iterate over a table', function()
         local e = enumerator({1, 2, 3})
         local i = 1
 
@@ -15,5 +15,48 @@ describe('Enumerators', function()
 
             i = i + 1
         end
+    end)
+
+    it('should iterate over a generator', function()
+        local g = function()
+            coroutine.yield(1)
+            coroutine.yield(2)
+            coroutine.yield(3)
+        end
+        local e = enumerator(g)
+        local i = 1
+
+        for v in iter(e) do
+            assert.are.same(i, v)
+
+            i = i + 1
+        end
+    end)
+
+    it('should eagerly evaluate enumerators', function()
+        local e = enumerator({1, 2, 3})
+        local t = e:totable()
+        
+        assert.are.same({1, 2, 3}, t)
+    end)
+
+    it('should take from another enumerator', function()
+        local e = enumerator({1, 2, 3, 4, 5})
+
+        local a = e:take(4)
+        local b = a:take(2)
+
+        local t = b:totable()
+
+        assert.are.same({1, 2}, t)
+    end)
+
+    it('should map to another enumerator', function()
+        local e = enumerator({1, 2, 3, 4})
+        local m = e:map(function(v) return v * 2 end)
+
+        local t = m:totable()
+
+        assert.are.same({2, 4, 6, 8}, t)
     end)
 end)
