008ef5ec3bdfec92be0ff9fb36a7e8271576fd2b
bbielsa <bgb7@njit.edu>
2019-03-20 15:39:54 -0400

    Removed test code from lush.lua

--- src/lush.lua
+++ src/lush.lua
@@ -5,15 +5,4 @@ local enumerator = require('enumerator').enumerator
 local iter = require('enumerator').iter
 local fs = require('luvco.fs').fs
 
-function double(x)
-  return 69, x*2
-end
 
-array = enumerator({2,3,4,5})
-map = array:map(double)
-
-for k,v, i in iter(map) do
-  print(k,v,i)
-end
-
-uv.run('default')
