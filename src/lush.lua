92806e8273f424c7e0f919c39a231330ed70891e
bbielsa <bgb7@njit.edu>
2020-04-09 23:02:14 -0400

    Add example usage of ls command

--- src/lush.lua
+++ src/lush.lua
@@ -5,4 +5,11 @@ local enumerator = require('enumerator').enumerator
 local iter = require('enumerator').iter
 local fs = require('luvco.fs').fs
 
+local ls = require('ls').cmd
+
+for file, node_type in iter(ls('/')) do
+    print(file, node_type)
+end
+
+uv.run('default')
 
