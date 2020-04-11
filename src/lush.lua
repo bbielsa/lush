3ccbff3b9b5982bae8dd2dfa39e296c0b2fa9e4e
bbielsa <bgb7@njit.edu>
2020-04-11 18:44:47 -0400

    Add example of printing a random number

--- src/lush.lua
+++ src/lush.lua
@@ -5,17 +5,26 @@ local enumerator = require('enumerator').enumerator
 local iter = require('enumerator').iter
 local fs = require('foundation.fs')
 local random = require('cmd.core.random')
+local taskprinter = require('cmd.output.taskprinter')
+local pipeline = require('cmd.pipeline')
 
 
-local cmd = random({from=0, to=10})
-cmd:process()
+local context = {}
+local randomcmd = random()
+local taskprinter = taskprinter()
+local commands = {randomcmd, taskprinter}
+local pipe = pipeline(commands)
 
+pipe:execute(context)
+
+--[[
 local entries = fs.scandir('/')
     :map(function(p) return fs.stat('/' .. p) end)
 
 for i, entry in iter(entries) do
     print(entry.path, entry.size)
 end
+]]
 
 uv.run('default')
 
