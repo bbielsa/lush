4afb2e19f19dede4c686f42cfcf1bbec7df8b638
bbielsa <bgb7@njit.edu>
2020-04-10 20:51:24 -0400

    Add example of iterating over files in a directory

--- src/lush.lua
+++ src/lush.lua
@@ -3,12 +3,16 @@ require('packages')
 local uv = require('luv')
 local enumerator = require('enumerator').enumerator
 local iter = require('enumerator').iter
-local fs = require('luvco.fs').fs
+local fs = require('foundation.fs')
 
-local ls = require('ls').cmd
 
-for file, node_type in iter(ls('/')) do
-    print(file, node_type)
+local entries = fs.scandir('/')
+    :filter(function(name) return name:find("^s") end)
+    :skip(1)
+    :take(3)
+
+for i, name in iter(entries) do
+    print(name)
 end
 
 uv.run('default')
